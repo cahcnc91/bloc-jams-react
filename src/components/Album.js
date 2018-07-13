7a98e674320c0800b9dff4ab5da2a267e84d4117
Camila Coder <camilanierocosta@hotmail.com>
2018-07-12 23:08:06 -0400

    added next button functionality

--- src/components/Album.js
+++ src/components/Album.js
@@ -78,6 +78,14 @@ class Album extends Component {
       this.play();
     }
 
+    handleNextClick() {
+      const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+      const newIndex = Math.min(currentIndex + 1, this.state.album.songs.length - 1);
+      const newSong = this.state.album.songs[newIndex];
+      this.setSong(newSong);
+      this.play();
+    }
+
     render() {
 
      return (
@@ -118,6 +126,7 @@ class Album extends Component {
          currentSong={this.state.currentSong} 
          handleSongClick={() => this.handleSongClick(this.state.currentSong)}
          handlePrevClick={() => this.handlePrevClick()}
+         handleNextClick={() => this.handleNextClick()}
          />
        </section>
      );
