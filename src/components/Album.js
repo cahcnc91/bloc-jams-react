6d781556a2795ed92330f880e34330c54c9b02a5
Camila Coder <camilanierocosta@hotmail.com>
2018-07-12 21:57:40 -0400

    added player bar, play and pause functionalities and previous track

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import albumData from './../data/albums';
 import IsMusicPlaying from './subcomponent/IsMusicPlaying';
+import PlayerBar from './PlayerBar';
 
 class Album extends Component {
   constructor(props) {
@@ -69,7 +70,15 @@ class Album extends Component {
         });
     }
 
-   render() {
+    handlePrevClick() {
+      const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+      const newIndex = Math.max(0, currentIndex - 1);
+      const newSong = this.state.album.songs[newIndex];
+      this.setSong(newSong);
+      this.play();
+    }
+
+    render() {
 
      return (
        <section className="album">
@@ -96,7 +105,6 @@ class Album extends Component {
               <tr className="song" key={index} onClick={() => this.handleSongClick(song)} >
                 <td key={index} onMouseEnter={() => this.hoverOnIt(index)} onMouseLeave={this.hoverOffIt}>
                     {this.state.isHovered && this.state.currentIndexHovered === index? <IsMusicPlaying isPlaying={this.state.isPlaying}/> : <button>{index+1}</button> }
-                   
                 </td>
                 <td>{song.title}</td>
                 <td>{song.duration}</td>
@@ -105,6 +113,12 @@ class Album extends Component {
             }
            </tbody>
          </table> 
+         <PlayerBar 
+         isPlaying={this.state.isPlaying} 
+         currentSong={this.state.currentSong} 
+         handleSongClick={() => this.handleSongClick(this.state.currentSong)}
+         handlePrevClick={() => this.handlePrevClick()}
+         />
        </section>
      );
    }
